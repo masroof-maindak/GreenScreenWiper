00212be81ebd4bf22d3a85562210b375cac0cded
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-01 03:27:22 +0500

    feat: lab conversion + channel extraction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <print>
-#include <string_view>
 
+#include <opencv2/core.hpp>
 #include <opencv2/core/cvstd_wrapper.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 
-constexpr std::string_view OUTPUT_FNAME{"../processed.png"};
+/*
+ * Since string_view doesn't work for the likes of namedWindow & showImage, and
+ * the small-string optimisation probably applies anyway.
+ */
+constexpr std::string INPUT_WINNAME{"Input"};
+constexpr std::string OUTPUT_WINNAME{"Output"};
+constexpr std::string OUTPUT_FNAME{"processed.png"};
+
+constexpr int LAB_A_CHAN_IDX{1};
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -14,19 +22,38 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
+	cv::Mat img{cv::imread(argv[1], cv::IMREAD_COLOR_BGR)};
 	if (img.data == nullptr) {
 		std::println(stderr, "Failed to read/parse image. Exiting");
 		return 2;
 	}
 
-	cv::namedWindow("Input", cv::WINDOW_FREERATIO);
-	// cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
+	// Convert image to lab
+	cv::Mat lab_img{};
+	cv::cvtColor(img, lab_img, cv::COLOR_BGR2Lab);
 
-	cv::imshow("Input", img);
+	// Extract green-red channel
+	cv::Mat_<cv::Vec<uchar, 1>> a_channel{};
+	cv::extractChannel(lab_img, a_channel, LAB_A_CHAN_IDX);
 
-	cv::waitKey(0);
+	int count{};
+	for (const auto &pxl : a_channel) {
+		// FIXME: doesn't work.
+		// if (pxl(0) < 0)
+		// 	count++;
 
-	cv::destroyAllWindows();
+		count++;
+	}
+
+	std::println("Count: {}", count);
+
+	// cv::namedWindow(INPUT_WINNAME, cv::WINDOW_FREERATIO);
+	// cv::namedWindow(OUTPUT_WINNAME, cv::WINDOW_FREERATIO);
+
+	// cv::imshow(INPUT_WINNAME, img);
+	// cv::imshow(OUTPUT_WINNAME, a_channel);
+	// cv::waitKey(0);
+
+	// cv::destroyAllWindows();
 	return 0;
 }
