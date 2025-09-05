50b131f640f9d9f874e81edac67b6dfc3268024a
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-06 04:45:16 +0500

    feat: locked in

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # GreenScreenWiper
 
-Piss shrimple Computer Vision project that takes a green-screen image as input and outputs a PNG with the green-screen removed.
+Piss shrimple Computer Vision project that takes a green-screen image and a background as input and outputs another image with the subject of the green-screen object overlayed onto the background. I haven't tested it for images of different sizes but it'll work. Probably.
+
+<p align="center">
+  <img alt="Wazir Khan Mosque - Lahore" src=".github/assets/bg.jpg" width="49%">
+  <img alt="Tyrannosaurus Rex" src=".github/assets/fg.jpg" width="49%">
+  <img alt="Tyrannosaurus Rex" src=".github/assets/result.jpg" width="75%">
+</p>
 
 ## Usage
 
@@ -10,7 +16,8 @@ cd GreenScreenWiper
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. # Or 'Debug' if you want to hack away.
 cmake --build .
-./gsw <fpath>
+
+./gsw <input-img-path> <background-img-path> <output-img-path>
 ```
 
 ## Dependencies
--- main.cpp
+++ main.cpp
@@ -1,68 +1,55 @@
-#include <opencv2/core/types.hpp>
 #include <print>
 
-#include <opencv2/core.hpp>
-#include <opencv2/core/cvstd_wrapper.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 
-/*
- * Since string_view doesn't work for the likes of namedWindow & showImage, and
- * the small-string optimisation probably applies anyway.
- */
-constexpr std::string INPUT_WINNAME{"Input"};
-constexpr std::string OUTPUT_WINNAME{"Output"};
-constexpr std::string OUTPUT_FNAME{"processed.png"};
-
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::println(stderr, "Usage: {} <input-img-path>", argv[0]);
+	if (argc != 4) {
+		std::println(stderr,
+					 "Usage: {} <input-img-path> <background-img-path> "
+					 "<output-img-path>",
+					 argv[0]);
 		return 1;
 	}
 
+	// Load images
 	cv::Mat fg_img{cv::imread(argv[1], cv::IMREAD_COLOR_BGR)};
 	if (fg_img.data == nullptr) {
 		std::println(stderr, "Failed to read/parse foreground. Exiting");
 		return 2;
 	}
 
-	// TODO: get from argument
-	cv::Mat bg_img{cv::imread("../bg.jpg", cv::IMREAD_COLOR_BGR)};
+	cv::Mat bg_img{cv::imread(argv[2], cv::IMREAD_COLOR_BGR)};
 	if (fg_img.data == nullptr) {
 		std::println(stderr, "Failed to read/parse background. Exiting");
 		return 3;
 	}
 
-	// Convert images to HSV
-	cv::Mat hsv_fg{};
-	cv::cvtColor(fg_img, hsv_fg, cv::COLOR_BGR2HSV);
+	// Resize background to match foreground (if needed)
+	if (fg_img.size() != bg_img.size())
+		cv::resize(bg_img, bg_img, fg_img.size());
 
-	cv::Mat hsv_bg{};
-	cv::cvtColor(fg_img, hsv_bg, cv::COLOR_BGR2HSV);
+	// Convert images to HSV
+	cv::Mat fg_hsv{};
+	cv::cvtColor(fg_img, fg_hsv, cv::COLOR_BGR2HSV);
+
+	cv::Mat bg_hsv{};
+	cv::cvtColor(bg_img, bg_hsv, cv::COLOR_BGR2HSV);
 
 	// Generate mask for green colours in HSV
 	cv::Mat mask{};
 	cv::Scalar lowerb{36, 0, 0};
 	cv::Scalar upperb{86, 255, 255};
-	cv::inRange(hsv_fg, lowerb, upperb, mask);
+	cv::inRange(fg_hsv, lowerb, upperb, mask);
 
-	// Merge two images
-	cv::Mat extracted{};
-	// FIXME
-	cv::bitwise_and(hsv_fg, hsv_bg, extracted, mask);
-	cv::copyTo(hsv_fg, hsv_bg, mask);
+	// Create inverse of mask and use it to keep the fg subject
+	cv::Mat mask_inv{};
+	cv::bitwise_not(mask, mask_inv);
+	fg_hsv.copyTo(bg_hsv, mask_inv);
 
-	cv::namedWindow(INPUT_WINNAME, cv::WINDOW_AUTOSIZE);
-	cv::namedWindow(OUTPUT_WINNAME, cv::WINDOW_FREERATIO);
+	// Convert back to BGR to save
+	cv::Mat result{};
+	cv::cvtColor(bg_hsv, result, cv::COLOR_HSV2BGR);
+	cv::imwrite(argv[3], result);
 
-	cv::imshow(INPUT_WINNAME, fg_img);
-	cv::imshow(OUTPUT_WINNAME, hsv_fg);
-	cv::waitKey(0);
-
-	cv::imwrite("plesae.jpg", hsv_bg);
-	cv::imwrite("plesae2.jpg", extracted);
-
-	cv::destroyAllWindows();
 	return 0;
 }
